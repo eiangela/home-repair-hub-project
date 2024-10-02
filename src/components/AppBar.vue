9416b2160bf7dba888c7dba8e1502d08a0586c01
Angela R <angelaribeiro2@hotmail.com>
2024-10-02 17:45:06 -0300

    WIP

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,16 +1,27 @@
 <template>
-  <v-app-bar :elevation="0" class="pa-3">
-      <v-row>
-        <v-col class="d-flex justify-md-center">
-          <img src="../assets/images/default-logo.png" alt="Logo" />
-        </v-col>
+  <v-app-bar :elevation="0" >
+    <v-row>
+      <v-col class="d-flex align-center justify-center">
+        <img src="../assets/images/default-logo.png" class="logo-bar" alt="Logo" />
+      </v-col>
 
-        <v-col class="d-flex justify-md-center">
-          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com">Home</v-btn>
-          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com">Team</v-btn>
-          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com">About Us</v-btn>
-          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com">Landing</v-btn>
-        </v-col>
-      </v-row>
+      <v-col class="d-flex justify-center">
+        <v-app-bar-nav-icon class="d-md-none"></v-app-bar-nav-icon>
+        <div class="d-none d-sm-none d-md-block">
+          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com"
+            >Home</v-btn
+          >
+          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com"
+            >Team</v-btn
+          >
+          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com"
+            >About Us</v-btn
+          >
+          <v-btn class="font-weight-bold menu_btn" vairant="text" href="https://www.google.com"
+            >Landing</v-btn
+          >
+        </div>
+      </v-col>
+    </v-row>
   </v-app-bar>
 </template>
--- src/components/RepairIntro.vue
+++ src/components/RepairIntro.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="repair-container d-flex align-center">
-    <div class="image-container">
-      <div class="hero-image"></div>
-    </div>
+  <div class="repair-container d-flex flex-column flex-column-reverse flex-md-row align-center">
+    <img src="../assets/images/eeee-min.jpg" class="hero-image" alt="" />
 
-    <div>
-        <div class="title-container">
-          <h1 class="repair-title mb-5">Reliable, Trusted, and Professional Services</h1>
-        </div>
+    <div class="pa-10 pa-md-2 pb-0">
+      <div class="title-container">
+        <h1 class="repair-title mb-md-0 font-weight-bold">
+          Reliable, Trusted, and Professional Services
+        </h1>
+      </div>
       <v-container>
         <v-row>
-          <v-col cols="12" md="6">
+          <v-col
+            cols="12"
+            sm="6"
+            md="12"
+            lg="4"
+            class="d-sm-flex justify-sm-center justify-md-start align-sm-center pb-md-0 pb-lg-3"
+          >
             <v-checkbox :model-value="true" persistent-hint readonly hide-details>
               <template v-slot:label>
                 <div class="checkbox-label font-weight-bold">Office</div>
@@ -18,7 +24,13 @@
             </v-checkbox>
           </v-col>
 
-          <v-col cols="12" md="6">
+          <v-col
+            cols="12"
+            sm="6"
+            md="12"
+            lg="6"
+            class="d-flex justify-sm-center justify-md-start align-sm-center pb-md-0 pb-lg-3"
+          >
             <v-checkbox :model-value="true" persistent-hint readonly hide-details>
               <template v-slot:label>
                 <div class="checkbox-label font-weight-bold">Kitchen</div>
@@ -26,15 +38,27 @@
             </v-checkbox>
           </v-col>
 
-          <v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            md="12"
+            lg="4"
+            class="d-flex justify-sm-center justify-md-start align-sm-center pb-lg-3 pb-md-0"
+          >
             <v-checkbox :model-value="true" persistent-hint readonly hide-details>
               <template v-slot:label>
-                <div class="checkbox-label font-weight-bold">Living Room</div>
+                <div class="checkbox-label font-weight-bold">Living</div>
               </template>
             </v-checkbox>
           </v-col>
 
-          <v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            md="12"
+            lg="6"
+            class="d-flex justify-sm-center justify-md-start align-sm-center pb-md-0 pb-lg-3 mb-md-5"
+          >
             <v-checkbox :model-value="true" persistent-hint readonly hide-details>
               <template v-slot:label>
                 <div class="checkbox-label font-weight-bold">Basement</div>
@@ -42,9 +66,11 @@
             </v-checkbox>
           </v-col>
         </v-row>
-        <v-btn class="repair-btn border-thin mt-8" width="250" height="65" elevation="8" size="x-large" rounded="0"
-          >Contact Us</v-btn
+        <v-col
+          class="d-flex justify-sm-center justify-md-start align-sm-center mt-sm-5 pt-md-0 pt-lg-3"
         >
+          <v-btn class="repair-btn" elevation="8" rounded="0">Contact Us</v-btn>
+        </v-col>
       </v-container>
     </div>
   </div>
